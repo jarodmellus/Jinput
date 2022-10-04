9c744034170a50d7a779eea6e2f7a7252a1a8083
jarodmellus <jmellus6899@gmail.com>
2022-10-04 10:46:52 -0400

    Created Repo

---
+++ Input.cs
@@ -0,0 +1,18 @@
+public static class Input {
+
+    //STANDARD ENGLISH
+    public enum KEYBOARD_INPUT {
+        ESCAPE,
+        BACKTICK,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,ZERO,DASH,EQUALS,BACKSPACE,
+        TAB,    Q,W,E,R,T,Y,U,I,O,P,    OPEN_SQUARE,CLOSE_SQUARE,BACK_SLASH,
+        CAPS,   A,S,D,F,G,H,J,K,L,  COLON,QUOTE,R_ENTER,
+        L_SHIFT,    Z,X,C,V,B,N,M,  COMMA,PERIOD,FORWARD_SLASH,R_SHIFT,
+        L_CTRL,L_ALT,SPACE,R_ALT,R_CTRL
+
+    }
+
+    public struct InputB {
+        
+    }
+
+}
